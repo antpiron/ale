c008a51434c3b5ff8ef74665d515566fc3a358b2
Anthony PIRON <anthony.piron@skynet.be>
2014-08-04 18:03:22 +0200

    Untested parser

--- src/exparser.c
+++ src/exparser.c
@@ -8,6 +8,15 @@ get_token(void *cls)
   return (struct ep_token) { .token = EXPR_TOK_EOF, .value = NULL};
 }
 
+static int 
+consume(struct ep_parser *ep)
+{
+  ep->token = ep->get_token(ep->cls);
+  ERROR_CUSTOM_RET(-1 == ep->token.token, -1, EXPR_ERR_LEX);
+
+  return ERR_SUCCESS;
+}
+
 int 
 expr_parser_init(struct ep_parser *ep)
 {
@@ -19,6 +28,7 @@ expr_parser_init(struct ep_parser *ep)
     {
       ep->binop[i] = NULL;
       ep->unop[i] = NULL;
+      ep->leaf[i] = NULL;
     }
 
   return ERR_SUCCESS;
@@ -46,7 +56,8 @@ expr_set_lexer(struct ep_parser *ep, struct ep_token (*get_token)(void *cls), vo
 int 
 expr_binop_add(struct ep_parser *ep, struct ep_binop *binop)
 {
-  ERROR_UNDEF_RET(binop->token >= EXPR_MAX_OP, -1);
+  ERROR_CUSTOM_RET(binop->token >= EXPR_MAX_OP, -1, EXPR_ERR_RANGE);
+  ERROR_CUSTOM_RET(EXPR_ASSOC_LEFT != binop->associativity && EXPR_ASSOC_RIGHT != binop->associativity, -1, EXPR_ERR_RANGE);
 
   ep->binop[binop->token] = binop;
 
@@ -57,16 +68,95 @@ int
 expr_unop_add(struct ep_parser *ep, struct ep_unop *unop)
 {
 
-  ERROR_UNDEF_RET(unop->token >= EXPR_MAX_OP, -1);
+  ERROR_CUSTOM_RET(unop->token >= EXPR_MAX_OP, -1, EXPR_ERR_RANGE);
 
   ep->unop[unop->token] = unop;
 
   return ERR_SUCCESS;
 }
 
+int 
+expr_leaf_add(struct ep_parser *ep, struct ep_leaf *leaf)
+{
+  ERROR_CUSTOM_RET(leaf->token >= EXPR_MAX_OP, -1, EXPR_ERR_RANGE);
+
+  ep->leaf[leaf->token] = leaf;
+
+  return ERR_SUCCESS;
+}
+
+static void* expr(struct ep_parser *ep, int p);
+static void* prod(struct ep_parser *ep);
+
+
 void* 
 expr_parse(struct ep_parser *ep)
 {
+  void *t = expr(ep, 0);
 
-  return NULL;
+  ERROR_CUSTOM_RET(EXPR_TOK_EOF != ep->token.token, NULL, EXPR_ERR_NEOF); // TODO: Clean t
+   
+  return t;
+}
+
+static void*
+expr(struct ep_parser *ep, int p)
+{
+  void *t = prod(ep);
+
+  ERROR_RET(NULL == t, NULL);
+
+  while (ep->token.token < EXPR_MAX_OP && NULL != ep->binop[ep->token.token])
+    {
+      struct ep_binop *op;
+      int q;
+      void *t1;
+      
+      op = ep->binop[ep->token.token];
+      if (op->precedence < p)
+	break;
+
+      ERROR_RET(-1 == consume(ep), NULL); // TODO: Clean t
+      
+      q = op->precedence;
+      if (EXPR_ASSOC_LEFT == op->associativity)
+	q++;
+
+      ERROR_RET(NULL == (t1 = expr(ep, q)), NULL); // TODO: Clean t
+
+      t = op->func(op, t, t1);
+    }
+
+  return t;
+}
+
+static void*
+prod(struct ep_parser *ep)
+{
+  void *t = NULL;
+
+  if (ep->token.token < EXPR_MAX_OP && NULL != ep->unop[ep->token.token])
+    {
+      struct ep_unop *op = ep->unop[ep->token.token];
+
+      ERROR_RET(-1 == consume(ep), NULL);
+      ERROR_RET(NULL == (t = expr(ep, op->precedence)), NULL);
+    }
+  else if (EXPR_TOK_LPAR == ep->token.token)
+    {
+      ERROR_RET(-1 == consume(ep), NULL);
+      ERROR_RET(NULL == (t = expr(ep, 0)), NULL);
+      ERROR_CUSTOM_RET(EXPR_TOK_RPAR != ep->token.token, NULL, EXPR_ERR_NLPAR); // TODO: Clean t
+    }
+  else if (ep->token.token < EXPR_MAX_OP && NULL != ep->leaf[ep->token.token])
+    {
+      struct ep_leaf *leaf = ep->leaf[ep->token.token];
+	    
+      t = leaf->func(leaf);
+      ERROR_RET(-1 == consume(ep), NULL); // TODO: Clean t
+    }
+  else
+    ERROR_CUSTOM_RET(1, NULL, EXPR_ERR_UTOK);
+
+  return t;
 }
--- src/exparser.h
+++ src/exparser.h
@@ -1,8 +1,24 @@
 #ifndef __EXPARSER_H
 #define __EXPARSER_H
 
-#define EXPR_TOK_EOF 0
+enum {
+  EXPR_TOK_EOF = 0,
+  EXPR_TOK_LPAR,
+  EXPR_TOK_RPAR,
+  EXPR_TOK_LAST
+};
+
 #define EXPR_MAX_OP 128
+
+#define EXPR_ASSOC_LEFT 0
+#define EXPR_ASSOC_RIGHT 0
+
+#define EXPR_ERR_NEOF 1
+#define EXPR_ERR_UOP 2
+#define EXPR_ERR_LEX 3
+#define EXPR_ERR_RANGE 4
+#define EXPR_ERR_UTOK 5
+#define EXPR_ERR_NLPAR 6
 
 struct ep_token {
   int token;
@@ -12,14 +28,19 @@ struct ep_token {
 struct ep_unop {
   int token;
   int precedence;
-  void* (*func)(void *right);
+  void* (*func)(struct ep_unop *op, void *right);
 };
 
 struct ep_binop {
   int token;
   int precedence;
   int associativity;
-  void* (*func)(void *left, void *right);
+  void* (*func)(struct ep_binop *op, void *left, void *right);
+};
+
+struct ep_leaf {
+  int token;
+  void* (*func)(struct ep_leaf *op);
 };
 
 struct ep_parser {
@@ -27,6 +48,7 @@ struct ep_parser {
   void *cls;
   struct ep_unop *unop[EXPR_MAX_OP];
   struct ep_binop *binop[EXPR_MAX_OP];
+  struct ep_leaf *leaf[EXPR_MAX_OP];
   struct ep_token (*get_token)(void *cls);  
 };
 
@@ -35,6 +57,7 @@ int expr_parser_destroy(struct ep_parser *ep);
 int expr_set_lexer(struct ep_parser *ep, struct ep_token (*get_token)(void *cls), void *cls);
 int expr_binop_add(struct ep_parser *ep, struct ep_binop *binop);
 int expr_unop_add(struct ep_parser *ep, struct ep_unop *unop);
+int expr_leaf_add(struct ep_parser *ep, struct ep_leaf *leaf);
 
 void* expr_parse(struct ep_parser *ep);
 
