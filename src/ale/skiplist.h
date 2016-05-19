64711ef2894fd7e4ccc3685a2ed26a2e9c44b162
Anthony PIRON <anthony.piron@skynet.be>
2016-05-19 23:04:21 +0200

    Add test (failed)

--- src/ale/skiplist.h
+++ src/ale/skiplist.h
@@ -4,15 +4,17 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-#include <ale/portability.h>
-
-
 #define SKL_INIT(name,maxlevel,keytype,valuetype,cmp_func)		\
   static inline size_t							\
   skl_randomlevel()							\
   {									\
-									\
-    return 0;								\
+    uint64_t rnd = rand();						\
+    size_t lvl = 0;							\
+    									\
+    while ( 1ull == (rnd & 1) && lvl < maxlevel )			\
+      { lvl++; rnd >>= 1; }						\
+    									\
+    return lvl+1;							\
   }									\
 									\
   struct skl_##name##_node						\
@@ -26,7 +28,7 @@
   skl_##name##_makeNode(size_t newLevel, keytype key, valuetype value)	\
   {									\
     struct skl_##name##_node *node = malloc(sizeof(struct skl_##name##_node)); \
-    ERROR_UNDEF_RET(NULL == node, NULL);				\
+    ERROR_UNDEF_FATAL(NULL == node, "skl_makeNode() Failed to allocate memory\n");	\
 									\
     node->key = key;							\
     node->value = value;						\
@@ -36,7 +38,7 @@
 									\
   struct skl_##name							\
   {									\
-    size_t level;							\
+    ssize_t level;							\
     struct skl_##name##_node header;					\
   };									\
 									\
@@ -44,13 +46,40 @@
   skl_##name##_init(struct skl_##name *skl)				\
   {									\
     skl->level = 0;							\
-    skl->header.forward[0] = NULL;					\
+    for (ssize_t i = 0 ; i < maxlevel ; i++)				\
+      skl->header.forward[i] = NULL;					\
   }								 	\
   									\
   static inline int							\
   skl_##name##_search(struct skl_##name *skl, keytype key,		\
-		      struct skl_##name##_node **node,			\
-		      struct skl_##name##_node *update[maxlevel])	\
+		      valuetype *value)					\
+  {									\
+    struct skl_##name##_node *x = &skl->header;				\
+    int cmp = -2;							\
+									\
+    for (ssize_t i = skl->level ; i > -1 ; i--)				\
+      {									\
+	while (NULL != x->forward[i]					\
+	       && -1 == (cmp = cmp_func(key,x->forward[i]->key)) )	\
+	  x = x->forward[i];						\
+      }									\
+									\
+    if (0 == cmp)							\
+      {									\
+	*value = x->forward[0]->value;					\
+	return 1;							\
+      }									\
+    else if (-2 == cmp)							\
+      return -1;							\
+									\
+    *value = x->value;								\
+    return 0;								\
+  }									\
+									\
+  static inline int							\
+  skl_##name##_search_update(struct skl_##name *skl, keytype key,	\
+			     struct skl_##name##_node **node,		\
+			     struct skl_##name##_node *update[maxlevel]) \
   {									\
     struct skl_##name##_node *x = &skl->header;				\
     int cmp = -1;							\
@@ -58,7 +87,7 @@
     for (ssize_t i = skl->level ; i > -1 ; i--)				\
       {									\
 	while (NULL != x->forward[i]					\
-	       && -1 == (cmp = cmp_func(key,x->forward[i]->key)) ) \
+	       && -1 == (cmp = cmp_func(key,x->forward[i]->key)) )	\
 	  x = x->forward[i];						\
 	update[i] = x;							\
       }									\
@@ -69,7 +98,7 @@
 	return 1;							\
       }									\
 									\
-    *node = x;								\
+    *node = x;					\
     return 0;								\
   }									\
 									\
@@ -80,7 +109,7 @@
     struct skl_##name##_node *x;					\
     struct skl_##name##_node *update[maxlevel];				\
 									\
-    if ( skl_##name##_search(skl, key, &x, update) )			\
+    if ( skl_##name##_search_update(skl, key, &x, update) )		\
       {									\
 	x->value = value;						\
       }									\
@@ -89,12 +118,12 @@
 	size_t newLevel = skl_randomlevel();				\
 	if (newLevel > skl->level)					\
 	  {								\
-	    for (ssize_t i = newLevel-1 ; i >= skl->level ; i--)	\
+	    for (ssize_t i = newLevel-1 ; i >=  skl->level ; i--) \
 	      update[i] = &skl->header;					\
 	    skl->level = newLevel;					\
 	  }								\
 	x = skl_##name##_makeNode(newLevel, key, value);		\
-	for (size_t i = 0 ; i < newLevel ; i++)				\
+	for (ssize_t i = 0 ; i < newLevel ; i++)			\
 	  {								\
 	    x->forward[i] = update[i]->forward[i];			\
 	    update[i]->forward[i] = x;					\
--- test/t-skiplist.c
+++ test/t-skiplist.c
@@ -19,7 +19,30 @@ int
 main(int argc, char *argv[argc])
 {
   struct skl_int skl;
+  int val;
 
+  skl_int_init(&skl);
+  skl_int_insert(&skl, 1, 1);
+  ERROR_UNDEF_FATAL_FMT(1 != skl.header.forward[0]->value,
+			"FATAL: 1 != %d\n", skl.header.forward[0]->value);
+  ERROR_UNDEF_FATAL(NULL != skl.header.forward[0]->forward[0],
+		    "FATAL: NULL != skl.header.forward[0]->forward[0]\n");
+  
+  skl_int_search(&skl,1,&val);
+  ERROR_UNDEF_FATAL_FMT(1 != val,
+			"FATAL: skl_int_search() 1 != %d\n", val);
+
+  for (int i = 0 ; i < 10 ; i++)
+    skl_int_insert(&skl, i, i);
+
+  for (int i = 0 ; i < 10 ; i++)
+    {
+      int ret = skl_int_search(&skl,i,&val);
+      ERROR_UNDEF_FATAL_FMT(1 != ret, "FATAL: skl_int_search() %d != 1\n", ret);
+      ERROR_UNDEF_FATAL_FMT(i != val,
+			    "FATAL: skl_int_search() %d != %d\n", i, val);
+    }
+			
   
   return EXIT_SUCCESS;
 }
