a844235d78b109a8e125292b0e926f95d91f8c93
Anthony Piron <anthony.piron@skynet.be>
2018-10-11 17:00:51 +0200

    Add stat module

--- src/ale/stats.h
+++ src/ale/stats.h
@@ -5,6 +5,10 @@
 
 double stats_mean(size_t n, const double x[n]);
 double stats_var(size_t n, const double x[n]);
+double stats_sd(size_t n, const double x[n]);
 double stats_cov(size_t n, const double x[n], const double y[n]);
+double stats_pearson_corr(size_t n, const double x[n], const double y[n]);
+void stats_mat_cov(size_t m, size_t n, double cov[m][m], const double x[m][n]);
+
 
 #endif
--- src/stats.c
+++ src/stats.c
@@ -1,10 +1,11 @@
+#include <math.h>
 #include "ale/stats.h"
 
 double
 stats_mean(size_t n, const double x[n])
 {
   double s = 0;
-  
+
   for (size_t i = 0 ; i < n ; i++)
     s += x[i];
 
@@ -27,6 +28,13 @@ stats_var(size_t n, const double x[n])
 }
 
 double
+stats_sd(size_t n, const double x[n])
+{
+  return sqrt(stats_var(n, x));
+}
+
+
+double
 stats_cov(size_t n, const double x[n], const double y[n])
 {
   double mx = stats_mean(n, x);
@@ -37,4 +45,50 @@ stats_cov(size_t n, const double x[n], const double y[n])
     s += (x[i] - mx) * (y[i] - my);
 
   return s / (n - 1);
+}
+
+double
+stats_pearson_corr(size_t n, const double x[n], const double y[n])
+{
+  double mx = stats_mean(n, x);
+  double my = stats_mean(n, y);
+  double cov = 0, sx = 0, sy = 0;
+
+  for (size_t i = 0 ; i < n ; i++)
+    {
+      double xterm = x[i] - mx;
+      double yterm = y[i] - my;
+      cov += xterm * yterm;
+      sx += xterm * xterm;
+      sy += yterm * yterm;
+    }
+  cov /= n - 1;
+  sx = sqrt(sx / (n - 1));
+  sy = sqrt(sy / (n - 1));
+
+// Shieh, G. Behavior Research Methods (2010) 42: 906. https://doi.org/10.3758/BRM.42.4.906
+  double prod = sx * sy;
+  double r = prod?0:cov / prod;
+  double rs = 1-r*r;
+  double rho = r * (1 + rs / (2 * (n - 2)) + 9 * rs * rs / (8 * n * (n - 2)));
+
+  return rho;
+}
+
+void
+stats_mat_cov(size_t m, size_t n, double cov[m][m], const double x[m][n])
+{
+  double means[m];
+
+  for (size_t i = 0 ; i < m ; i++)
+    means[i] = stats_mean(n, x[i]);
+
+  for (size_t i = 0 ; i < m ; i++)
+    for (size_t j = i ; j < m ; j++)
+      {
+	double s = 0;
+	for (size_t k = i ; k < n ; k++)
+	  s += (x[i][k] - means[i] ) * (x[j][k] - means[j]);
+	cov[j][i] = cov[i][j] = s;
+      }
 }
