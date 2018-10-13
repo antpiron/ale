179c92426b9ccba53aa6f00d9ccd9ed224861b19
Anthony Piron <anthony.piron@skynet.be>
2018-10-13 17:39:57 +0200

    Add stat module

--- test/t-stats-norm-rand-std.c
+++ test/t-stats-norm-rand-std.c
@@ -9,22 +9,21 @@ main(int argc, char *argv[argc])
 {
 #define LEN (100000)
   double x[LEN] = {0.0};
-  double eps = 0.01, res, delta;
+  double eps = 0.01, res, exp, delta;
 
   for (int i = 0 ; i < LEN ; i++)
     {
       res = x[i] = stats_unif_rand_std();
-      ERROR_UNDEF_FATAL_FMT(0 > res, "FAIL: mean(stats_unif_rand()) == %f < 0\n", res);
-      ERROR_UNDEF_FATAL_FMT(1 < res, "FAIL: mean(stats_unif_rand()) == %f > 1\n", res);
+      ERROR_UNDEF_FATAL_FMT(0 > res, "FAIL: mean(stats_unif_rand_std()) == %f < 0\n", res);
+      ERROR_UNDEF_FATAL_FMT(1 < res, "FAIL: mean(stats_unif_rand_std()) == %f > 1\n", res);
     }
-  for (int i = 0 ; i < LEN ; i++)
-    x[i] = stats_norm_rand_std();
   res = stats_mean(LEN, x);
-  delta = fabs(0.0 - res);
-  ERROR_UNDEF_FATAL_FMT(delta >= eps, "FAIL: mean(stats_unif_rand_std()) == %f != 0.0\n", res);
+  delta = fabs(0.5 - res);
+  ERROR_UNDEF_FATAL_FMT(delta >= eps, "FAIL: mean(stats_unif_rand_std()) == %f != 0.5\n", res);
   res = stats_var(LEN, x);
-  delta = fabs(1.0 - res);
-  ERROR_UNDEF_FATAL_FMT(delta >= eps, "FAIL: var(stats_unif_rand_std()) == %f != 0.0\n", res);
+  exp = 1.0 / 12.0;
+  delta = fabs(exp - res);
+  ERROR_UNDEF_FATAL_FMT(delta >= eps, "FAIL: var(stats_unif_rand_std()) == %f != %f\n", res, exp);
 
 
   return EXIT_SUCCESS;
