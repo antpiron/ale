aeec99c68a13194391097eccb97e5453a029232a
Anthony Piron <anthony.piron@skynet.be>
2018-10-19 14:42:24 +0200

    Add t-test test

---
+++ test/t-stats-t-test.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <math.h>
+
+#include "ale/error.h"
+#include "ale/stats.h"
+
+#define ORDER 16
+
+int
+main(int argc, char *argv[argc])
+{
+#define LEN (10000)
+  double x[LEN] = {0.0};
+  double res, exp, delta;
+  struct stats_t_test data;
+  double eps = 0.01;
+
+  for (int i = 0 ; i < LEN ; i++)
+    x[i] = stats_student_rand(LEN-1);
+
+  res = stats_t_test(LEN, x, 0, 0, &data); 
+  double m = stats_mean(LEN, x);
+  ERROR_UNDEF_FATAL_FMT(res < eps, "FAIL: stats_t_test() == %f < 0.5. Mean = %f \n",
+			res, m);
+  
+  
+  return EXIT_SUCCESS;
+}
