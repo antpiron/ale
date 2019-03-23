224cc014241d3b2132baaf331c5d412c60a4ff3d
Anthony Piron <anthony.piron@skynet.be>
2019-03-23 11:39:15 +0100

    Add missing process files

---
+++ src/ale/process.h
@@ -0,0 +1,8 @@
+#ifndef __PROCESS_H
+#define __PROCESS_H
+
+#include <stdio.h>
+
+FILE* process_popenv(const char *pathname, char *const argv[]);
+
+#endif
---
+++ src/process.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+#include "ale/process.h"
+#include "ale/error.h"
+
+FILE*
+process_popenv(const char *pathname, char *const argv[])
+{
+  int pipefd[2];
+  pid_t pid;
+  FILE *file;
+
+  ERROR_ERRNO_RET(-1 == pipe(pipefd), NULL);
+  ERROR_ERRNO_GOTO(-1 == (pid = fork()), ERROR_FORK);
+
+  // child
+  if (0 == pid)
+    {
+      close(pipefd[0]);
+      ERROR_ERRNO_FATAL(-1 == dup2(pipefd[1], STDOUT_FILENO),
+			"process_openv(): dup2 failed.\n");
+      ERROR_ERRNO_FATAL(-1 == execv(pathname, argv),
+			"process_openv(): execv failed.\n");
+      _exit(EXIT_SUCCESS); // never here
+    }
+
+  file = fdopen(pipefd[0],"r");
+  ERROR_ERRNO_GOTO(NULL == file, ERROR_FDOPEN);
+  close(pipefd[1]);
+  
+  return file;
+  
+ ERROR_FDOPEN:
+ ERROR_FORK:
+  close(pipefd[0]);
+  close(pipefd[1]);
+  ERROR_RET(1, NULL);
+}
