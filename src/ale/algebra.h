429b76aea52672f67a20356e1b6111cfec85ddc0
Anthony Piron <anthony.piron@ulb.ac.be>
2020-01-05 22:55:14 +0100

    Add test

--- src/ale/algebra.h
+++ src/ale/algebra.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 
+double* alg_mul_m_m(size_t m, size_t n, size_t p, const double A[m][n], const double B[n][p], double res[m][p]);
+
 int alg_Ux_b_solve(size_t n, const double U[n][n], const double b[n], double x[n]);
 int alg_QR_Qtb_householder(size_t m, size_t n, size_t p, double A[m][n], double B[m][p]);
 
--- test/t-alg-qr-qtb-householder.c
+++ test/t-alg-qr-qtb-householder.c
@@ -4,21 +4,40 @@
 #include "ale/error.h"
 #include "ale/algebra.h"
 
+
+void
+print_m(size_t m, size_t n, double A[m][n])
+{
+  for (size_t i = 0 ; i < m ; i++)
+    {
+      for (size_t j = 0 ; j < n ; j++)
+	printf("%f\t", A[i][j]);
+      printf("\n");
+    }
+}
+
 int
 main(int argc, char *argv[argc])
 {
-  const size_t m = 2, n = 2, p = 2;
-  double A[2][2] = { {1, 1},
-		     {1, 2}} ;
-  double B[2][2] = { {1, 0},
-		     {0, 1}};
+  const size_t m = 3, n = 3, p = 3;
+  double A[3][3] = { {1, 1, 1},
+		     {1, 2, 2},
+		     {1, 2, 3}} ;
+  double B[3][3] = { {1, 0, 0},
+		     {0, 1, 0},
+		     {0, 0, 1}};
+  double C[3][3];
 
   alg_QR_Qtb_householder(m, n, p, A, B);
-  printf("%f %f\n", A[0][0], A[0][1]);
-  printf("%f %f\n", A[1][0], A[1][1]);
+  print_m(m, n, A);
   printf("\n");
-  printf("%f %f\n", B[0][0], B[0][1]);
-  printf("%f %f\n", B[1][0], B[1][1]);
+  print_m(m, n, B);
+
+  alg_mul_m_m(m,n,p, B, A, C);
+  printf("\n");
+  print_m(m, n, C);
+
+
   // res = ale_beta(1, 1);
   // delta = fabs(res - 1);
   // ERROR_UNDEF_FATAL_FMT(delta >= eps, "FAIL: ale_beta(1,1) == %f != 1\n",
