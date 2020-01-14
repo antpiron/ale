2b1f93e25d7d04945c4ee13bf980e1ea4b4a6a8f
Anthony Piron <anthony.piron@ulb.ac.be>
2020-01-14 18:42:45 +0100

    Add functionnal ux_b_solve

--- src/algebra.c
+++ src/algebra.c
@@ -221,6 +221,7 @@ print_m(size_t m, size_t n, double A[m][n])
 void
 householder_proj(size_t m, size_t n, size_t k, double v[k], double A[m][n])
 {
+  // TODO: dynamic alloc. or param
   double vA[n];
   
   // v^t * A_k:m,k:n
