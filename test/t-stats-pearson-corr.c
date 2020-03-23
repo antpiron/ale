ca7c4fcad24ae8e1816ec49e8d06666015443bb0
Anthony Piron <anthony.piron@ulb.ac.be>
2020-03-23 13:46:33 +0100

    Add fix pearson test

--- test/t-stats-pearson-corr.c
+++ test/t-stats-pearson-corr.c
@@ -4,12 +4,11 @@
 #include "ale/error.h"
 #include "ale/stats.h"
 
-#define ORDER 16
 
 int
 main(int argc, char *argv[argc])
 {
-#define LEN (40000)
+#define LEN (100000)
   double *x = malloc(sizeof(double) * LEN);
   double *y = malloc(sizeof(double) * LEN);
   double res, exp, delta, pvalue;
@@ -30,32 +29,32 @@ main(int argc, char *argv[argc])
 
   ret = stats_pearson_corr(LEN, x, y, &res, &pvalue);
   ERROR_UNDEF_FATAL_FMT(-1 == ret,
-			"FAIL: stats_stats_pearson_corr() == %d != 0\n", ret);
+			"FAIL 1: stats_stats_pearson_corr() == %d != 0\n", ret);
   exp = 1.0;
   delta = fabs(exp - res);
   ERROR_UNDEF_FATAL_FMT(delta >= eps,
-			"FAIL: stats_pearson_corr() == %f != %f. delta = %f\n",
+			"FAIL 2: stats_pearson_corr() == %f != %f. delta = %f\n",
 			res, exp, delta);
   exp = 0.0;
   delta = fabs(exp - pvalue);
   ERROR_UNDEF_FATAL_FMT(delta >= eps,
-			"FAIL: stats_pearson_corr().pval == %f != 0\n", pvalue);
+			"FAIL 3: stats_pearson_corr().pval == %f != 0\n", pvalue);
   
   for (long i = 0 ; i < LEN ; i++)
     {
-      x[i] = stats_unif_rand(0,100);
-      y[i] = stats_unif_rand(0,100);
+      x[i] = stats_unif_std_rand();
+      y[i] = stats_unif_std_rand();
     }
 
   ret = stats_pearson_corr(LEN, x, y, &res, &pvalue);
-  ERROR_UNDEF_FATAL_FMT(-1 == ret, "FAIL: stats_stats_pearson_corr() == %d != 0\n", ret);
+  ERROR_UNDEF_FATAL_FMT(-1 == ret, "FAIL 4: stats_stats_pearson_corr() == %d != 0\n", ret);
   exp = 0.0;
   delta = fabs(exp - res);
   ERROR_UNDEF_FATAL_FMT(pvalue < 0.01 && delta >= eps,
-			"FAIL: stats_pearson_corr() == %f != %f. pval = %f\n",
+			"FAIL 5: stats_pearson_corr() == %f != %f. pval = %f\n",
 			res, exp, pvalue);
   ERROR_UNDEF_FATAL_FMT(pvalue < 0.01,
-			"FAIL: stats_pearson_corr().pval == %f < 0.05\n", pvalue);
+			"FAIL 6: stats_pearson_corr().pval == %f < 0.05\n", pvalue);
   
   free(x);
   free(y);
