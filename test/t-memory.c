917a44bd1b29972f26fc3a2eafdf09dcb0c1f34c
Anthony Piron <anthony.piron@ulb.ac.be>
2020-04-08 17:57:00 +0200

    Add memory

--- test/t-memory.c
+++ test/t-memory.c
@@ -5,7 +5,8 @@
 #include "ale/error.h"
 #include "ale/memory.h"
 
-
+#define ALLOC_SIZE ((1ul << 10) + 1)
+#define NUM_ITER (1ul << 8)
 
 int
 main(int argc, char *argv[argc])
@@ -13,15 +14,25 @@ main(int argc, char *argv[argc])
   struct mem_pool pool;
 
   mem_init(&pool);
-  ERROR_UNDEF_FATAL(MEM_DEFAULT_BLOCK_SIZE != pool.block_size, "FAIL: mem_init()" );
+  ERROR_UNDEF_FATAL(MEM_DEFAULT_BLOCK_SIZE != pool.block_size, "FAIL: mem_init()\n" );
 
-  for (int i = 0 ; i < 1024 ; i++)
+  for (int i = 0 ; i < NUM_ITER ; i++)
     {
       char *ptr = NULL;
-      ptr = mem_malloc(&pool, 1000);
-      memset(ptr, i % 256, 1000);
-      ERROR_UNDEF_FATAL(NULL == pool.stack.next, "FAIL: mem_malloc()" );
+      ptr = mem_malloc(&pool, ALLOC_SIZE);
+      memset(ptr, i, ALLOC_SIZE);
+      ERROR_UNDEF_FATAL(NULL == pool.stack.next, "FAIL: mem_malloc()\n" );
     }
+
+  size_t sum = 0;
+  for (struct sl_node *node = sl_first(&pool.stack) ;
+       node ; node = sl_next(node) )
+    {
+      struct mem_block* block = node->data;
+      sum += block->usage;
+    }
+  size_t exp =  ALLOC_SIZE * NUM_ITER;
+  ERROR_UNDEF_FATAL_FMT(exp != sum, "FAIL: mem_malloc() usage = %zu != %z\n", sum, exp );  
   
   mem_destroy(&pool);
   
