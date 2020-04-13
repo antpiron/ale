dbdc66d49d2544f73483b01b444fe9b38e651a69
Anthony Piron <anthony.piron@ulb.ac.be>
2020-04-13 18:31:30 +0200

    Add poisson rand generation

--- src/ale/stats.h
+++ src/ale/stats.h
@@ -44,7 +44,6 @@ double stats_binom_F(long k, long n, double p);
 double stats_poisson_f(long k, double lambda);
 double stats_poisson_F(long k, double lambda);
 unsigned long stats_poisson_rand_its(double lambda);
-unsigned long stats_poisson_rand_ptrd(double lambda);
 unsigned long stats_poisson_rand_pa(double lambda);
 unsigned long stats_poisson_rand(double lambda);
 
