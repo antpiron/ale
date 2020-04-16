91cbf869ce34cc5a270e0cfb7395f24d3950d818
Anthony Piron <anthony.piron@ulb.ac.be>
2020-04-16 15:33:44 +0200

    Fix normaization zeroes

--- src/stats-normalize.c
+++ src/stats-normalize.c
@@ -16,12 +16,20 @@ stats_normalize_samples_ls(size_t m, size_t n, size_t r,
   if ( STATS_WEIGHT_VARIANCE & options )
     {
       for (size_t i = 0 ; i < r ; i++)
-	w[i] = 1.0d / stats_var(n, mat[ref[i]]);
+	{
+	  double var = stats_var(n, mat[ref[i]]);
+	  ERROR_UNDEF_RET(0 == var, -1);
+	  w[i] = 1.0d / var;
+	}
     }
   else
     {
       for (size_t i = 0 ; i < r ; i++)
-	w[i] = 1.0d / stats_mean(n, mat[ref[i]]);
+	{
+	  double mean = stats_mean(n, mat[ref[i]]);
+	  ERROR_UNDEF_RET(0 == mean, -1);
+	  w[i] = 1.0d / mean;
+	}
     }
 
   // TODO: switch order of loop for cache
@@ -31,10 +39,15 @@ stats_normalize_samples_ls(size_t m, size_t n, size_t r,
       double num = 0;
       for (size_t i = 0 ; i < r ; i++)
 	{
-	  double x =  mat[i][s];
+	  double x =  mat[ref[i]][s];
 	  num += x;
 	  denom += w[i] * x* x ;
 	}
+      ERROR_UNDEF_MSG_FMT(0 == denom,
+			  "WARNING in stats_normalize_samples_ls():"
+			  "unable to compute beta[%zu] sum_r(mat[r, %zu]) == 0\n",
+			  s, s);
+      // ERROR_UNDEF_RET(0 == denom, -1);
       beta[s] = num / denom;
     }
 
@@ -61,8 +74,12 @@ int stats_normalize_samples_poisson(size_t m, size_t n, size_t r,
       double num = 0;
       for (size_t i = 0 ; i < r ; i++)
 	{
-	  double x = mat[i][s];
-	  ERROR_UNDEF_RET(0 == x, -1);
+	  double x = mat[ref[i]][s];
+	  ERROR_UNDEF_MSG_FMT(0 == denom,
+			      "WARNING in stats_normalize_samples_poisson():"
+			      "unable to compute beta[%zu] mat[%zu, %zu] == 0\n",
+			      s, i, s);
+	  // ERROR_UNDEF_RET(0 == x, -1);
 	  num += mumu[i] / x;
 	  denom += x;
 	}
