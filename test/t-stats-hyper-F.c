38bda3baec47d27073bb31cba7a48b3420bbbb53
Anthony Piron <anthony.piron@ulb.ac.be>
2020-08-17 18:59:14 +0200

    Reorganize

--- test/t-stats-hyper-F.c
+++ test/t-stats-hyper-F.c
@@ -7,7 +7,7 @@
 int
 main(int argc, char *argv[argc])
 {
-  double eps = 0.000001, res, delta;
+  double eps = 0.0000001, res, delta;
 
   res = stats_hyper_F(5, 5, 5, 10);
   ERROR_UNDEF_FATAL(isnan(res), "FAIL: stats_hyper_F(5, 5, 5, 10) == NaN\n");
@@ -38,6 +38,11 @@ main(int argc, char *argv[argc])
   ERROR_UNDEF_FATAL(isnan(res), "FAIL: stats_hyper_F(2, 5, 5, 10) == NaN\n");
   delta = fabs(0.5 - res);
   ERROR_UNDEF_FATAL_FMT(delta >= eps, "FAIL: stats_hyper_F(2, 5, 5, 10) == %f != 0.5\n", res);
+
+  res = stats_hyper_F(0, 5, 1, 10000000);
+  ERROR_UNDEF_FATAL(isnan(res), "FAIL: stats_hyper_F(0, 5, 1, 10000000) == NaN\n");
+  delta = fabs(0.9999995 - res);
+  ERROR_UNDEF_FATAL_FMT(delta >= eps, "FAIL: stats_hyper_F(0, 5, 1, 10000000) == %f != 0.9999995\n", res);
 
   return EXIT_SUCCESS;
 }
