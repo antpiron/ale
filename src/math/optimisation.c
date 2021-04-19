1f197d8c24d04285afff082e7035c654cf3839c4
Anthony Piron <anthony.piron@ulb.ac.be>
2021-04-19 19:19:31 +0200

    Add  gradient descent

--- src/math/optimisation.c
+++ src/math/optimisation.c
@@ -12,7 +12,7 @@ enum {
 };
 
 #define GENERIC_FUNC(SUFFIX,TYPE)					\
-  /* https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm */ \
+  /* https://en.wikipedia.org/wiki/Gradient_descent */ \
   int optimisation_gradient_descent##SUFFIX(size_t n, TYPE x[n],	\
 					    TYPE (*f)(TYPE *x, void *cls), \
 					    void (*gradf)(TYPE *y, TYPE *x, void *cls), \
