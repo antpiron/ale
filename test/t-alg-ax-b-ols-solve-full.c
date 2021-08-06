1d854201f288f9f74d0a675a9a5761d19653a69f
Anthony Piron <anthony.piron@ulb.ac.be>
2021-08-06 11:20:51 +0200

    Fix F-statistic

--- test/t-alg-ax-b-ols-solve-full.c
+++ test/t-alg-ax-b-ols-solve-full.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <math.h>
+#include <stdarg.h>
 
 #include "ale/error.h"
 #include "ale/algebra.h"
@@ -12,17 +13,105 @@ const double alpha = 0.01;
 const double eps = 0.0000001;
 
 enum {
-      RANDOM_B,
-      PERFECT_B,
-      CORRELATED_B
+      B_MODE_RANDOM,
+      B_MODE_PERFECT,
+      B_MODE_CORRELATED
+};
+
+enum {
+      ARG_ERROR_SD,
+      ARG_A,
+      ARG_B,
+      ARG_INTERCEPT,
+      ARG_B_MODE,
+      ARG_TEST_COEF,
+      ARG_TEST_R2,
+      ARG_TEST_FSTAT,
+      ARG_TEST_FSTAT_PVALUE,
+      ARG_TEST_BETA_STAT,
+      ARG_TEST_BETA_PVALUE,
+      ARG_END
 };
 
 void
-gen_model(char *name, size_t m, size_t n, size_t p, size_t intercept, int rand_B, double var_e, void *A_param, void *B_param)
+gen_model(char *name, size_t m, size_t n, size_t p, ...)
 {
+  va_list ap;
   int ret;
   struct alg_ols ols;
   double A[m][n],  X[n][p], B[m][p], X_res[n][p];
+  size_t intercept = 1;
+  int rand_B = B_MODE_RANDOM;
+  double error_sd = 10;
+  void *A_param = NULL;
+  void *B_param = NULL;
+  double (*coef_exp)[p] = NULL;
+  double *r2_exp = NULL;
+  double *fstat_exp = NULL;
+  double *pvalue_exp = NULL;
+  double (*beta_stat_exp)[p] = NULL;
+  double (*beta_pvalue_exp)[p] = NULL;
+
+  va_start(ap, p); 
+  for ( int cont = 1 ; cont ; )
+    {
+      int arg_type = va_arg(ap, int);
+      
+      if (ARG_END == arg_type)
+	break;
+      
+      switch (arg_type)
+	{
+	case ARG_INTERCEPT:
+	  intercept = va_arg(ap, size_t);
+	  break;
+	  
+	case ARG_B_MODE:
+	  rand_B = va_arg(ap, int);
+	  break;
+	  
+	case ARG_ERROR_SD:
+	  error_sd = va_arg(ap, double);
+	  break;
+
+	case ARG_A:
+	  A_param = va_arg(ap, void*);
+	  break;
+
+	case ARG_B:
+	  B_param = va_arg(ap, void*);
+	  break;
+
+	case ARG_TEST_COEF:
+	  coef_exp = va_arg(ap, void*);
+	  break;
+	  
+	case ARG_TEST_R2:
+	  r2_exp = va_arg(ap, void*);
+	  break;
+	  
+	case ARG_TEST_FSTAT:
+	  fstat_exp = va_arg(ap, void*);
+	  break;
+
+	case ARG_TEST_FSTAT_PVALUE:
+	  pvalue_exp = va_arg(ap, void*);
+	  break;
+	  
+	case ARG_TEST_BETA_STAT:
+	  beta_stat_exp = va_arg(ap, void*);
+	  break;
+
+	case ARG_TEST_BETA_PVALUE:
+	  beta_pvalue_exp = va_arg(ap, void*);
+	  break;
+	  
+	default:
+	  cont = 0;
+	  break;
+	}
+    }
+  va_end(ap);
 
   printf("\n\n%s\n", name);
 
@@ -35,17 +124,19 @@ gen_model(char *name, size_t m, size_t n, size_t p, size_t intercept, int rand_B
   if ( B_param )
     memcpy(B, B_param, sizeof(double) * m * p);
   else
-  if ( RANDOM_B == rand_B )
-    ALG_INIT_M(m, p, B, stats_unif_rand(0, 1000) );
-  else
     {
-      ALG_INIT_M(n, p, X_res, stats_unif_rand(0, 1000) );
-      
-      alg_mul_m_m(m, n, p, A, X_res, B);
- 
-      if ( CORRELATED_B  == rand_B)
+      if ( B_MODE_RANDOM == rand_B )
+	ALG_INIT_M(m, p, B, stats_unif_rand(0, 1000) );
+      else
 	{
-	  ALG_ADD_M(m, p, B, stats_norm_rand(0, var_e) );
+	  ALG_INIT_M(n, p, X_res, stats_unif_rand(0, 1000) );
+	  
+	  alg_mul_m_m(m, n, p, A, X_res, B);
+	  
+	  if ( B_MODE_CORRELATED  == rand_B)
+	    {
+	      ALG_ADD_M(m, p, B, stats_norm_rand(0, error_sd) );
+	    }
 	}
     }
 
@@ -53,7 +144,7 @@ gen_model(char *name, size_t m, size_t n, size_t p, size_t intercept, int rand_B
   print_m(m, n, A);
   printf("\nB=\n");
   print_m(m, p, B);
-  if ( RANDOM_B != rand_B)
+  if ( B_MODE_RANDOM != rand_B)
     {
       printf("\nX_res=\n");
       print_m(n, p, X_res);
@@ -65,7 +156,7 @@ gen_model(char *name, size_t m, size_t n, size_t p, size_t intercept, int rand_B
   printf("\nX=\n");
   print_m(n, p, X);
 
-  if ( PERFECT_B  == rand_B )
+  if ( B_MODE_PERFECT  == rand_B )
     {
       for (size_t i = 0 ; i < n ; i++)
 	for (size_t j = 0; j < p ; j++)
@@ -76,10 +167,78 @@ gen_model(char *name, size_t m, size_t n, size_t p, size_t intercept, int rand_B
 	  }
     }
 
-  
+  if ( NULL != coef_exp )
+    {
+      for (size_t i = 0; i < n ; i++)
+	for (size_t j = 0; j < p ; j++)
+	  {
+	    ERROR_UNDEF_FATAL_FMT(0 != ale_cmp_double(X[i][j], coef_exp[i][j], eps),
+				  "FAIL: %s alg_AX_B_OLS_solve_full() X[%ld, %ld] = %f != %f = coef_exp[%ld, %ld]\n",
+				  name, i, j, X[i][j], coef_exp[i][j], i, j);
+	  }
+    }
+
   ret = alg_AX_B_OLS_statistics(&ols, intercept);
   ERROR_UNDEF_FATAL_FMT(ret < 0, "FAIL: %s alg_AX_B_OLS_statistics() ret = %d\n != 0", name, ret);
 
+  if ( NULL != r2_exp )
+    {
+      for (size_t i = 0; i < p ; i++)
+	{
+	  double *r2 = ols.r_squared;
+	  ERROR_UNDEF_FATAL_FMT(0 != ale_cmp_double(r2[i], r2_exp[i], eps),
+				"FAIL: %s alg_AX_B_OLS_solve_full() r2[%ld] = %f != %f = r2_exp[%ld]\n",
+				name, i, r2[i], r2_exp[i], i);
+	}
+    }
+
+  if ( NULL != fstat_exp )
+    {
+      for (size_t i = 0; i < p ; i++)
+	{
+	  double *score = ols.score;
+	  ERROR_UNDEF_FATAL_FMT(0 != ale_cmp_double(score[i], fstat_exp[i], eps),
+				"FAIL: %s alg_AX_B_OLS_solve_full() score[%ld] = %f != %f = fstat_exp[%ld]\n",
+				name, i, score[i], fstat_exp[i], i);
+	}
+    }
+
+  if ( NULL != pvalue_exp )
+    {
+      for (size_t i = 0; i < p ; i++)
+	{
+	  double *pvalue = ols.pvalue;
+	  ERROR_UNDEF_FATAL_FMT(0 != ale_cmp_double(pvalue[i], pvalue_exp[i], eps),
+				"FAIL: %s alg_AX_B_OLS_solve_full() pvalue[%ld] = %f != %f = pvalue_exp[%ld]\n",
+				name, i, pvalue[i], pvalue_exp[i], i);
+	}
+    }
+
+  if ( NULL != beta_stat_exp )
+    {
+      for (size_t i = 0; i < n ; i++)
+	for (size_t j = 0; j < p ; j++)
+	  {
+	    double (*score)[p] = ols.beta_score;
+	    ERROR_UNDEF_FATAL_FMT(0 != ale_cmp_double(score[i][j], beta_stat_exp[i][j], eps),
+				  "FAIL: %s alg_AX_B_OLS_solve_full() beta_stat[%ld, %ld] = %f != %f = beta_stat_exp[%ld, %ld]\n",
+				  name, i, j, score[i][j], beta_stat_exp[i][j], i, j);
+	  }
+    }
+
+  if ( NULL != beta_pvalue_exp )
+    {
+      for (size_t i = 0; i < n ; i++)
+	for (size_t j = 0; j < p ; j++)
+	  {
+	    double (*pvalue)[p] = ols.beta_pvalue;
+	    ERROR_UNDEF_FATAL_FMT(0 != ale_cmp_double(pvalue[i][j], beta_pvalue_exp[i][j], eps),
+				  "FAIL: %s alg_AX_B_OLS_solve_full() beta_pvalue[%ld, %ld] = %f != %f = beta_pvalue_exp[%ld, %ld]\n",
+				  name, i, j, pvalue[i][j], beta_pvalue_exp[i][j], i, j);
+	  }
+    }
+
+  
   printf("\nAX=\n");
   print_m(m, p, ols.AX);
   printf("\nmeans=\n");
@@ -98,6 +257,12 @@ gen_model(char *name, size_t m, size_t n, size_t p, size_t intercept, int rand_B
 
   printf("\nF-pval=\n");
   print_v(p, ols.pvalue);
+
+  printf("\nbeta-stat=\n");
+  print_m(n, p, ols.beta_score);
+
+  printf("\nbeta-pvalue=\n");
+  print_m(n, p, ols.beta_pvalue);
 
   alg_AX_B_OLS_destroy(&ols);
 }
@@ -131,22 +296,46 @@ main(int argc, char *argv[argc])
 
      model <- lm(B ~ A)
      summary(model)
-     sum(model$residuals^2) 
+     
+Call:
+lm(formula = B ~ A)
 
+Residuals:
+     1      2      3      4      5      6 
+-173.2  494.3 -149.2 -183.5 -216.6  228.2 
+
+Coefficients:
+            Estimate Std. Error t value Pr(>|t|)
+(Intercept) 752.2431   419.0238   1.795    0.170
+A1           -0.5003     0.6670  -0.750    0.508
+A2            0.2487     1.1021   0.226    0.836
+
+Residual standard error: 378.3 on 3 degrees of freedom
+Multiple R-squared:  0.1691,	Adjusted R-squared:  -0.3848 
+F-statistic: 0.3053 on 2 and 3 DF,  p-value: 0.7574
+
+     sum(model$residuals^2) 
      meanB <- mean(B)
      X  <- cbind(model$coefficients)
      AX  <- (cbind(1, A) %*% X)
      sum( (AX - meanB)^2 )
    */
 
-  gen_model("random with intercept", 6, 3, 1, 1, RANDOM_B, 0,
-	    (double[6][3]) { {1.000000,	751.411799, 53.406825},	
-			      {1.000000, 878.793326, 184.466027},
-				 {1.000000, 934.970735, 400.979769},
-				   {1.000000, 248.113489, 6.991620 },
-				     {1.000000, 969.135640, 443.855135},
-				     {1.000000, 397.544328, 330.078718 } },
-	    (double[6]) {216.396249, 852.721703, 234.933271, 446.330890, 161.112500, 863.669808});
+    gen_model("random with intercept", 6, 3, 1,
+	      ARG_INTERCEPT, 1,
+	      ARG_B_MODE, B_MODE_RANDOM, 
+	      ARG_A, (double[6][3]) { {1.000000, 751.411799, 53.406825},	
+				       {1.000000, 878.793326, 184.466027},
+					 {1.000000, 934.970735, 400.979769},
+					   {1.000000, 248.113489, 6.991620 },
+					     {1.000000, 969.135640, 443.855135},
+					       {1.000000, 397.544328, 330.078718 } },
+	      ARG_B, (double[6]) {216.396249, 852.721703, 234.933271, 446.330890, 161.112500, 863.669808},
+	      ARG_TEST_COEF, (double[3][1]) { {752.2431179}, {-0.5003319}, {0.2486852} }, 
+	      ARG_TEST_R2, (double[1]) { 0.1691208 },
+	      ARG_TEST_FSTAT, (double[1]) { 0.3053166 },
+	      ARG_TEST_FSTAT_PVALUE, (double[1]) { 0.7573678 },
+	      ARG_END);
 
   /* gen_model("random with intercept", 6, 3, 1, 1, RANDOM_B, 0, NULL, NULL);
   gen_model("perfect correlation with intercept", 6, 3, 1, 1, PERFECT_B, 0, NULL, NULL);
